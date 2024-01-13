8a9d7f062807d6801f9b1f39697ffc3db1f48e3f
Jesus Perez <jesusgperez16@gmail.com>
2024-01-13 16:00:09 -0500

    Making validations in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,18 +22,29 @@ class PostsController < ApplicationController
   # GET /posts/{id}
   def show
     @post = Post.find(params[:id])
-    render json: @post, status: :ok
+
+    if @post.published?
+      render json: @post, status: :ok
+      return
+    end
+
+    if Current.user && @post.user_id == Current.user.id
+      render json: @post, status: :ok
+      return
+    end
+
+    render json: {error: 'Not found'}, status: :not_found
   end
 
   # POST /posts
   def create
-    @post = Post.create!(create_params)
+    @post = Current.user.posts.create!(create_params)
     render json: @post, status: :created
   end
 
   # PUT /posts
   def update
-    @post = Post.find(params[:id])
+    @post = Current.user.posts.find(params[:id])
     @post.update!(update_params)
     render json: @post, status: :ok
   end
@@ -53,16 +64,19 @@ class PostsController < ApplicationController
     headers = request.headers
     unless headers['Authorization'].present?
       render json: {error: 'Unauthorized'}, status: :unauthorized
+      return
     end
 
     auth = headers['Authorization'].match(token_regex)
     if auth.nil?
       render json: {error: 'Unauthorized'}, status: :unauthorized
+      return
     end
     
     user = User.find_by_auth_token(auth[1])
     if user.nil?
       render json: {error: 'Unauthorized'}, status: :unauthorized
+      return
     end
 
     Current.user = user
