3d8319e964cf09dd89055b8d1edcbdc08a5cabe0
Jesus Perez <jesusgperez16@gmail.com>
2024-01-13 17:41:15 -0500

    Implementing some cache

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,9 @@
 class PostsSearchService
-    def self.search(posts, query)
-        posts.where("title like '%#{query}%'")
+  def self.search(posts, query)
+    posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hours) do
+      posts.where("title like '%#{query}%'").map(&:id)
     end
+
+    posts.where(id: posts_ids)
+  end
 end
